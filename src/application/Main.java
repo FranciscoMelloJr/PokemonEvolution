5b798df105fbb259c318ef02bda7f13cf25f7235
Francisco Mello <franciscomelloxpe@gmail.com>
2021-10-14 20:50:16 -0300

    icons

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import org.opencv.core.Core;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -16,6 +17,8 @@ public class Main extends Application {
 			System.loadLibrary( Core.NATIVE_LIBRARY_NAME);
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Principal.fxml"));
 			Scene scene = new Scene(root);
+			primaryStage.getIcons().add(new Image("file:src/banner/icon.png"));
+	        primaryStage.setTitle("What's that Pokémon ??");
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
